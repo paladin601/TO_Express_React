72afd438f184a7e3a1079d95a544d4249c2539c5
Leonardo Pinzon <leonardo3103@gmail.com>
2020-04-16 15:30:44 -0300

    init server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+//Setting
+app.set('port', process.env.PORT || 5001);
+
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+//Folders static
+
+app.listen(app.get('port'), () => {
+    console.log("server run port " + app.get('port'));
+})
