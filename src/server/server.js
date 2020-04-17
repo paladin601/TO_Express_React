50b2f85d05a5a4a5700b17cee6a6f992de8aa351
Leonardo Pinzon <leonardo3103@gmail.com>
2020-04-16 21:50:22 -0300

    add .env

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -14,7 +16,6 @@ app.use(express.json());
 //Routes
 app.use('/api/crud', require('./router/router'));
 //Folders static
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
     console.log("server run port " + app.get('port'));
