572cddd3f22d2f6c08a546871b9c67423e789e38
Leonardo Pinzon <leonardo3103@gmail.com>
2020-04-16 22:53:14 -0300

    add bootstrap

--- src/server/server.js
+++ src/server/server.js
@@ -14,7 +14,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Folders static
-let appStaticAssetsPath = path.join(__dirname, '../client')
+app.use('/modules', express.static(path.join(__dirname, '../../node_modules')));
+let appStaticAssetsPath = path.join(__dirname, '../client');
 //Routes
 //Routes Client
 require('../router/router-client')(express, app, appStaticAssetsPath);
